cd08e8535b1fe64a72b7e25aef41019a5b63835e
AryenMukundam <aryenmukundam@gmail.com>
2026-06-16 15:10:01 +0530

    Setup Github client using Octokit.

---
+++ backend/src/github/client.ts
@@ -0,0 +1,11 @@
+import {Octokit} from "@octokit/rest"
+import { config } from "../config.js"
+
+let octokit : Octokit | null = null
+
+export function getOctokit() : Octokit {
+    if(!octokit){
+        octokit = new Octokit({auth : config.githubToken})
+    }
+    return octokit
+}
